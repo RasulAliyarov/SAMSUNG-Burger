e26bae4bb644c8232f63e1e044b4b9f6f500bc80
ca r213.12 <ca.r213.12@ca.local>
2022-12-06 07:30:10 -0800

    add element & change theme

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const burgerBtn = document.getElementById("burgerBtn")
 const burgDiv = document.getElementsByClassName("burgDiv")[0]
+const themeBtn = document.getElementById("themeBtn")
+const app__body = document.getElementById("app__body")
+const elementtheme = document.getElementsByClassName("elementtheme")
+const addBtn = document.getElementById("addBtn")
+const cards = document.querySelector(".cards")
+const link = document.querySelectorAll(".link")
+
 let index = 0
+
+
 burgerBtn.addEventListener("click", () => {
     // if (index == 0) {
 
@@ -23,6 +32,57 @@ burgerBtn.addEventListener("click", () => {
         burgDiv.style.display = "flex"
         burgDiv.classList.add("active")
     }
+})
+
+
+themeBtn.addEventListener("click", () => {
+
+    if (app__body.classList.contains("dark")) {
+        app__body.classList.remove("dark")
+        app__body.style.backgroundColor = "white"
+        for (let i = 0; i < elementtheme.length; i++) {
+            elementtheme[i].style.backgroundColor = "rgb(14, 43, 138)"
+            elementtheme[i].style.color = "white"
+
+        }
+
+        link.forEach(item => item.style.color = "rgb(14, 43, 138)")
+
+    }
+    else {
+        app__body.style.backgroundColor = "rgb(14, 43, 138)"
+        app__body.classList.add("dark")
+
+        for (let i = 0; i < elementtheme.length; i++) {
+
+            elementtheme[i].style.backgroundColor = "white"
+            elementtheme[i].style.color = "rgb(14, 43, 138)"
+
+        }
+        link.forEach(item => item.style.color = "white")
+
+    }
+
+    console.log(link)
+})
+
+
+
+
+
+addBtn.addEventListener("click", () => {
+    cards.innerHTML += `
+    <div class="card" style="width: 18rem;">
+    <img src="./ae-fhd-t5300-ua40t5300auxzn-frontblack-231892019.webp" alt="">
+
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    </div>
+`
 })
 
 
